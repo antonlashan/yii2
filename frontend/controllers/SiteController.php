3e7932acc411dc31165e05a0e970fbfff301b66f
antonlashan2 <antonlashan@gmail.com>
2015-10-13 22:40:06 +0530

    dompdf

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -17,145 +17,117 @@ use kartik\mpdf\Pdf;
  */
 class SiteController extends Controller {
 
-	public $defaultAction = 'create';
+    public $defaultAction = 'create';
 
-	/**
-	 * @inheritdoc
-	 */
-	public function behaviors()
-	{
-		return [
-			'access' => [
-				'class' => AccessControl::className(),
-				'only' => ['logout', 'signup'],
-				'rules' => [
-					[
-						'actions' => ['signup'],
-						'allow' => true,
-						'roles' => ['?'],
-					],
-					[
-						'actions' => ['logout'],
-						'allow' => true,
-						'roles' => ['@'],
-					],
-				],
-			],
-			'verbs' => [
-				'class' => VerbFilter::className(),
-				'actions' => [
-					'logout' => ['post'],
-				],
-			],
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function actions()
-	{
-		return [
-			'error' => [
-				'class' => 'yii\web\ErrorAction',
-			],
-			'captcha' => [
-				'class' => 'yii\captcha\CaptchaAction',
-				'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-			],
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function actions() {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
 
-	/**
-	 * Creates a new User model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 * @return mixed
-	 */
-	public function actionCreate()
-	{
-		$model = new User();
-		$userDetail = new UserDetail(['scenario' => UserDetail::SCENARIO_REGISTRATION]);
+    /**
+     * Creates a new User model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate() {
+        $model = new User();
+        $userDetail = new UserDetail(['scenario' => UserDetail::SCENARIO_REGISTRATION]);
 
-		if (Yii::$app->request->post() && $model->load(Yii::$app->request->post()) && $userDetail->load(Yii::$app->request->post())) {
+        if (Yii::$app->request->post() && $model->load(Yii::$app->request->post()) && $userDetail->load(Yii::$app->request->post())) {
 
-			$model->is_admin = User::IS_ADMIN_NO;
-			$model->status = User::STATUS_INACTIVE;
+            $model->is_admin = User::IS_ADMIN_NO;
+            $model->status = User::STATUS_INACTIVE;
 
-			if ($model->save()) {
+            if ($model->save()) {
 
-				$userDetail->user_id = $model->id;
+                $userDetail->user_id = $model->id;
 
-				if ($userDetail->save()) {
-					$id = urlencode(base64_encode($model->id));
-					return $this->redirect(['view', 'id' => $id]);
-				}
-			}
-		}
+                if ($userDetail->save()) {
+                    $id = urlencode(base64_encode($model->id));
+                    return $this->redirect(['view', 'id' => $id]);
+                }
+            }
+        }
 
-		return $this->render('create-student', [
-				'model' => $model,
-				'userDetail' => $userDetail,
-		]);
-	}
+        return $this->render('create-student', [
+                    'model' => $model,
+                    'userDetail' => $userDetail,
+        ]);
+    }
 
-	public function actionView($id)
-	{
-		$decodedId = base64_decode(urldecode($id));
+    public function actionView($id) {
+        $decodedId = base64_decode(urldecode($id));
 
-		if (($user = User::findOne($decodedId)) === null) {
-			throw new NotFoundHttpException('The requested page does not exist.');
-		}
+        if (($user = User::findOne($decodedId)) === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
 
-		if (($batch = Batch::getCurrentBatch()) === null) {
-			throw new NotFoundHttpException('The requested page does not exist.');
-		}
+        if (($batch = Batch::getCurrentBatch()) === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
 
-		return $this->render('view', ['user' => $user, 'batch' => $batch, 'id' => $id]);
-	}
+        return $this->render('view', ['user' => $user, 'batch' => $batch, 'id' => $id, 'pdf' => false]);
+    }
 
-	public function actionPdf($id)
-	{
-		$id = base64_decode(urldecode($id));
+    public function actionPdf($id) {
+        $id = base64_decode(urldecode($id));
 
-		if (($user = User::findOne($id)) === null) {
-			throw new NotFoundHttpException('The requested page does not exist.');
-		}
+        if (($user = User::findOne($id)) === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
 
-		if (($batch = Batch::getCurrentBatch()) === null) {
-			throw new NotFoundHttpException('The requested page does not exist.');
-		}
+        if (($batch = Batch::getCurrentBatch()) === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
 
-		$content = $this->renderPartial('view', ['user' => $user, 'batch' => $batch]);
+        $content = $this->renderPartial('view', ['user' => $user, 'batch' => $batch, 'pdf' => true]);
 
-		// setup kartik\mpdf\Pdf component
-		$pdf = new Pdf([
-			// set to use core fonts only
-//			'mode' => Pdf::MODE_UTF8, 
-			// A4 paper format
-			'format' => Pdf::FORMAT_A4,
-			// portrait orientation
-			'orientation' => Pdf::ORIENT_PORTRAIT,
-			// stream to browser inline
-			'destination' => Pdf::DEST_DOWNLOAD,
-			// your html content input
-			'content' => $content,
-			// format content from your own css file if needed or use the
-			// enhanced bootstrap css built by Krajee for mPDF formatting 
-//			'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
-			'cssFile' => '',
-//			// any css to be embedded if required
-			'cssInline' => 'table {border-collapse: collapse;} table, td {border: 1px solid black; font-size: 14px;font-family: dejavuserif;}',
-//			// set mPDF properties on the fly
-//			'options' => ['charset_in' => 'windows-1252', ' allow_charset_conversion' => true],
-//			// call mPDF methods on the fly
-//			'methods' => [
-//				'SetHeader' => ['Krajee Report Header'],
-//				'SetFooter' => ['{PAGENO}'],
-//			]
-		]);
-
-		// return the pdf output as per the destination setting
-		return $pdf->render();
-	}
+        define('DOMPDF_ENABLE_AUTOLOAD', false);
+        require_once Yii::getAlias('@vendor') . '/dompdf/dompdf/dompdf_config.inc.php';
+        $dompdf = new \DOMPDF();
+        $dompdf->load_html($content);
+        $dompdf->render();
+        $dompdf->stream("sample.pdf");
+    }
 
 }
